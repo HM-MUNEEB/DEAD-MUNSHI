a55c550c1370b520319c764f9b42356696f4f81e
HM Muneeb <hm.muneeb.u.r@gmail.com>
2021-03-03 01:54:57 +0500

    Updates

---
+++ precache-manifest.2535e7b408e151412291b37320c42ec6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b91f374a7d38e5359ee0a5e9a31847",
+    "url": "/DEAD-MUNSHI/index.html"
+  },
+  {
+    "revision": "c9408cbdcad4956a8762",
+    "url": "/DEAD-MUNSHI/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "6170d49c5316c0af7a61",
+    "url": "/DEAD-MUNSHI/static/css/main.b5e28be5.chunk.css"
+  },
+  {
+    "revision": "c9408cbdcad4956a8762",
+    "url": "/DEAD-MUNSHI/static/js/2.16883552.chunk.js"
+  },
+  {
+    "revision": "6de8224ec00495266c752e3494b57712",
+    "url": "/DEAD-MUNSHI/static/js/2.16883552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6170d49c5316c0af7a61",
+    "url": "/DEAD-MUNSHI/static/js/main.bf399e23.chunk.js"
+  },
+  {
+    "revision": "bf54cf23f17d9fa7068e",
+    "url": "/DEAD-MUNSHI/static/js/runtime-main.bccb2239.js"
+  },
+  {
+    "revision": "2aae33ce8b13c88b3da9967cd7f3d71a",
+    "url": "/DEAD-MUNSHI/static/media/DEAD-MUNSHI.2aae33ce.png"
+  },
+  {
+    "revision": "2d05ef705191369fbc63b831a0e3a25c",
+    "url": "/DEAD-MUNSHI/static/media/bg - signUp.2d05ef70.png"
+  },
+  {
+    "revision": "ddb4d6621674922b18c39511fc878132",
+    "url": "/DEAD-MUNSHI/static/media/bg.ddb4d662.png"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/DEAD-MUNSHI/static/media/cancel.cad70081.svg"
+  },
+  {
+    "revision": "c0e4ffa80114edc2939699d5c43d0389",
+    "url": "/DEAD-MUNSHI/static/media/check.c0e4ffa8.svg"
+  },
+  {
+    "revision": "becc1a48493ab2785008f6722cd05ddf",
+    "url": "/DEAD-MUNSHI/static/media/facebook.becc1a48.svg"
+  },
+  {
+    "revision": "485aabcedbd48c5a6794e7ce6758b77a",
+    "url": "/DEAD-MUNSHI/static/media/instagram.485aabce.svg"
+  },
+  {
+    "revision": "a75a5f095bf3a1155c66d012b647ca4c",
+    "url": "/DEAD-MUNSHI/static/media/loading.a75a5f09.svg"
+  },
+  {
+    "revision": "98e0685c1cbb45dfffe1ea6c0ab3a8f2",
+    "url": "/DEAD-MUNSHI/static/media/loading_small_black.98e0685c.svg"
+  },
+  {
+    "revision": "e75855a18e3532ccada0d25685656a95",
+    "url": "/DEAD-MUNSHI/static/media/logout.e75855a1.svg"
+  },
+  {
+    "revision": "382f1e7d2a9c640e8929da4717bbd267",
+    "url": "/DEAD-MUNSHI/static/media/posts.382f1e7d.svg"
+  },
+  {
+    "revision": "8f3bc11aa87a695ca38e607f2f0444e2",
+    "url": "/DEAD-MUNSHI/static/media/reload.8f3bc11a.svg"
+  },
+  {
+    "revision": "ee5b2f48d6288fdab881a0b22a762ce7",
+    "url": "/DEAD-MUNSHI/static/media/user.ee5b2f48.svg"
+  }
+]);
